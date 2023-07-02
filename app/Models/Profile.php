0da0026276d4d89003befb648ee42c214274fab1
AndrewGamel <89712132+AndrewGamel@users.noreply.github.com>
2023-07-02 14:53:07 +0300

    .

--- app/Models/Profile.php
+++ app/Models/Profile.php
@@ -8,4 +8,16 @@ use Illuminate\Database\Eloquent\Model;
 class Profile extends Model
 {
     use HasFactory;
+    protected $primaryKey = 'user_id';
+    protected $fillable = [
+        'first_name','last_name','birthday','gender','image','city','street_address','state','postal_code','country','locale'];
 }
+/*
+
+            $table->enum(, ['male', 'female'])->nullable();
+            $table->string()->nullable();
+            $table->string()->nullable();
+            $table->string()->nullable();
+
+
+ */
